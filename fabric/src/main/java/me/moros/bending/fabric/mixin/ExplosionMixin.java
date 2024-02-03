308d52663028e07f8d1b0f818e4d57bb6290dce3
PrimordialMoros <mail@moros.me>
2024-02-03 16:09:30 +0200

    Fix explosion mixin that caused crash

--- fabric/src/main/java/me/moros/bending/fabric/mixin/ExplosionMixin.java
+++ fabric/src/main/java/me/moros/bending/fabric/mixin/ExplosionMixin.java
@@ -19,9 +19,9 @@
 
 package me.moros.bending.fabric.mixin;
 
-import java.util.Collections;
 import java.util.List;
 
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import me.moros.bending.fabric.event.ServerItemEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -29,31 +29,15 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.storage.loot.LootParams;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Explosion.class)
 public abstract class ExplosionMixin {
-  @Shadow
-  @Final
-  private Level level;
-
-  @Redirect(method = "finalizeExplosion", at = @At(value = "INVOKE",
-    target = "Lnet/minecraft/world/level/block/state/BlockState;getDrops(Lnet/minecraft/world/level/storage/loot/LootParams$Builder;)Ljava/util/List;")
-  )
-  private List<ItemStack> bending$blockDrops(BlockState state, LootParams.Builder builder) {
-    var stacks = state.getDrops(builder);
-    var pos = BlockPos.containing(builder.getParameter(LootContextParams.ORIGIN));
-    var result = ServerItemEvents.BLOCK_DROP_LOOT.invoker().onDropLoot((ServerLevel) level, pos, stacks);
-    if (result.getResult() != InteractionResult.FAIL) {
-      return result.getObject();
-    }
-    return Collections.emptyList();
+  @WrapWithCondition(method = "finalizeExplosion", at = @At(value = "INVOKE",
+    target = "Lnet/minecraft/world/level/block/Block;popResource(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/item/ItemStack;)V"))
+  private boolean bending$blockDrops(Level level, BlockPos pos, ItemStack itemStack) {
+    var result = ServerItemEvents.BLOCK_DROP_LOOT.invoker().onDropLoot((ServerLevel) level, pos, List.of(itemStack));
+    return result.getResult() != InteractionResult.FAIL;
   }
 }
