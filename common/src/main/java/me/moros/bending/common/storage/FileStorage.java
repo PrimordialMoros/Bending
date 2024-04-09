e844a4a0f382445a4715c715b9853ebb077aa621
PrimordialMoros <mail@moros.me>
2024-04-09 21:04:10 +0300

    Add concurrent limit to filestorage

--- common/src/main/java/me/moros/bending/common/storage/FileStorage.java
+++ common/src/main/java/me/moros/bending/common/storage/FileStorage.java
@@ -27,6 +27,7 @@ import java.time.Duration;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
@@ -47,6 +48,7 @@ final class FileStorage extends AbstractStorage {
   private final Path dataPath;
   private final Loader<?> loader;
   private final LoadingCache<Path, ReentrantLock> locks;
+  private final Semaphore semaphore;
 
   FileStorage(Logger logger, Path directory, Loader<?> loader) {
     super(logger);
@@ -58,6 +60,7 @@ final class FileStorage extends AbstractStorage {
       throw new UncheckedIOException(e);
     }
     this.locks = Caffeine.newBuilder().expireAfterAccess(Duration.ofMinutes(10)).build(key -> new ReentrantLock());
+    this.semaphore = new Semaphore(Math.max(Runtime.getRuntime().availableProcessors(), 2));
   }
 
   private Path filePath(UUID uuid) {
@@ -101,12 +104,14 @@ final class FileStorage extends AbstractStorage {
   private <R> R loadDataFile(Path path, IOFunction<ConfigurationReference<?>, R> function, R onException) {
     ReentrantLock lock = locks.get(path);
     lock.lock();
+    semaphore.acquireUninterruptibly();
     try (var ref = loader.load(path)) {
       return function.apply(ref);
     } catch (IOException e) {
       logger.warn(e.getMessage(), e);
       return onException;
     } finally {
+      semaphore.release();
       lock.unlock();
     }
   }
