0fee539a9f29f012897ec1615787acd4ad889135
PrimordialMoros <mail@moros.me>
2024-07-21 17:26:13 +0300

    Bump minor version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,4 @@ plugins {
 }
 
 group = "me.moros"
-version = "3.9.1-SNAPSHOT"
+version = "3.10.0-SNAPSHOT"
--- paper/src/main/java/me/moros/bending/paper/gui/ElementMenu.java
+++ paper/src/main/java/me/moros/bending/paper/gui/ElementMenu.java
@@ -1,5 +1,5 @@
 /*
- * Copy               right 2020-2024 Moros
+ * Copyright 2020-2024 Moros
  *
  * This file is part of Bending.
  *
